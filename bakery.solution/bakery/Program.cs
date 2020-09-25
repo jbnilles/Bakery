3d2d4104ad362ff7fa1a9d53b999b7b1704f16e0
jbnilles <jbnilles24@gmail.com>
2020-09-25 14:33:37 -0700

    complete overhaul of user interface and handle user input errors

--- bakery.solution/bakery/Program.cs
+++ bakery.solution/bakery/Program.cs
@@ -41,14 +41,23 @@ namespace Bakery
               }
               goto default;
             case "buy":
-              Console.WriteLine("Thank you for your order of:");
-              Console.WriteLine("You order has {0} Bread", numBread);
-              Console.WriteLine("You order has {0} Pasteries", numPasteries);
-              Console.WriteLine("Total Cost ${0} ", orderCost);
-              numBread = 0;
-              numPasteries = 0;
-              orderCost = 0;
-              break;
+              if(orderCost > 0)
+              {
+                Console.WriteLine("Thank you for your order of:");
+                Console.WriteLine("You order has {0} Bread", numBread);
+                Console.WriteLine("You order has {0} Pasteries", numPasteries);
+                Console.WriteLine("Total Cost ${0} ", orderCost);
+                numBread = 0;
+                numPasteries = 0;
+                orderCost = 0;
+                break;
+              }
+              else
+              {
+                Console.WriteLine("There was an error. You have no items in your order please. Please try again.");
+                break;
+              }
+              
             case "exit":
               break;
             default:
@@ -56,69 +65,6 @@ namespace Bakery
               break;
         }
       }
-      // Console.WriteLine("Welcome to Pierre's Bakery! Type Menu to bring up the menu, type order to place order or Type exit to exit the program");
-      // while((line = Console.ReadLine()).ToLower() != "exit")
-      // {
-      //   if(line.ToLower() == "menu")
-      //   {
-      //     Console.WriteLine("Bread: Buy 2, get 1 free. A single loaf costs $5.");
-      //     Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5.");
-      //   }
-      //   else if (line.ToLower() == "order") 
-      //   {
-      //     Console.WriteLine("Enter number of Bread:");
-      //     string strBread = Console.ReadLine();
-      //     int numBread;
-      //     int numPasteries;
-      //     if(int.TryParse(strBread, out numBread))
-      //     {
-      //       Console.WriteLine("Enter number of Pasteries:");
-      //       string strPasteries = Console.ReadLine();
-      //       if(int.TryParse(strPasteries, out numPasteries))
-      //       {
-      //         Bread bread = new Bread(numBread);
-      //         Pastery  pastery = new Pastery(numPasteries);
-      //         int orderCost = bread.getCost() + pastery.getCost();
-      //         Console.WriteLine("You ordered {0} Bread", numBread);
-      //         Console.WriteLine("You ordered {0} Pasteries", numPasteries);
-      //         Console.WriteLine("Total Cost ${0} ", orderCost);
-      //         Console.WriteLine();
-      //         Console.WriteLine("Type edit to change order or type buy to complete order and purchase.", orderCost);
-      //         if((line = Console.ReadLine()).ToLower() == "edit")
-      //         {
-      //           Console.WriteLine("edit");
-      //         }
-      //         else if (line.ToLower() == "buy")
-      //         {
-      //           Console.WriteLine("buy");
-      //         }
-      //         else
-      //         {
-      //           Console.WriteLine("user error");
-      //         }
-              
-              
-      //       }
-      //       else
-      //       {
-      //         Console.WriteLine("There was an error in your input. Please try again.");
-      //         Console.WriteLine(" Type Menu to bring up the menu, type order to place order or Type exit to exit the program");
-      //       }
-      //     }
-      //     else
-      //     {
-      //       Console.WriteLine("There was an error in your input. Please try again.");
-      //       Console.WriteLine(" Type Menu to bring up the menu, type order to place order or Type exit to exit the program");
-      //     }
-          
-
-      //   }
-      //   else
-      //     {
-      //       Console.WriteLine("There was an error in your input. Please try again.");
-      //       Console.WriteLine(" Type Menu to bring up the menu, type order to place order or Type exit to exit the program");
-      //     }
-      // }
     } 
   }
   
