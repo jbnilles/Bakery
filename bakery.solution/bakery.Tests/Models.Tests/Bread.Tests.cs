10fb8673cfe6044f7afd114bf10def1ab1f0dd2a
jbnilles <jbnilles24@gmail.com>
2020-09-25 11:59:39 -0700

    add more tests and plan new functioality to add

--- bakery.solution/bakery.Tests/Models.Tests/Bread.Tests.cs
+++ bakery.solution/bakery.Tests/Models.Tests/Bread.Tests.cs
@@ -7,6 +7,12 @@ namespace Bakery.Tests
   public class BreadTest
   {
     [TestMethod]
+       public void AmmountProperty_ReturnAmmount_int(){
+        Bread bread = new Bread(7);
+        Assert.AreEqual(7,bread.Amount);
+       //Assert.AreEqual("nineteen",Words.getNumWordRecursive(999999999999));
+      }
+    [TestMethod]
        public void getCost_ReturnCost_int(){
         Bread bread = new Bread(7);
         Assert.AreEqual(25,bread.getCost());
--- bakery.solution/bakery.Tests/Models.Tests/Pastery.Tests.cs
+++ bakery.solution/bakery.Tests/Models.Tests/Pastery.Tests.cs
@@ -6,6 +6,13 @@ namespace Bakery.Tests
   [TestClass]
   public class PasteryTest
   {
+
+      [TestMethod]
+       public void AmmountProperty_ReturnAmmount_int(){
+        Pastery pastery = new Pastery(7);
+        Assert.AreEqual(7,pastery.Amount);
+       //Assert.AreEqual("nineteen",Words.getNumWordRecursive(999999999999));
+      }
       [TestMethod]
        public void getCost_ReturnCost_int(){
         Pastery pastery = new Pastery(7);
